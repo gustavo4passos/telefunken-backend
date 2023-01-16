51cab730c6c30287099d8358bab90df83900ab3b
gustavo4passos <gustavo4passos@gmail.com>
2023-01-16 19:50:14 -0300

    Adds express http server

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import WebSocket from 'ws'
 import { ExtWebSocket } from './connection/socket'
 import { storage } from './game/storage'
 import { onMessage } from './messageHandlers'
-const wss = new WebSocket.Server({
-  port: process.env.PORT ? Number(process.env.PORT) : 7071
-})
+import express from 'express'
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 7071
+
+const httpServer = express()
+  .use((req, res) => {
+    res.send('Empty')
+  })
+  .listen(PORT, () => console.log(`HTTP server listening on port ${PORT}`))
+
+const wss = new WebSocket.Server({ server: httpServer })
 
 wss.on('connection', (ws: ExtWebSocket) => {
   ws.id = storage.AddConnection(ws)
+  console.log('Client connected, id: ', ws.id)
   // TODO: Terrible. Fix it. Find how to extend the class, if possible, of create a middleware
   ws.sendObject = <T>(data: T) => {
     ws.send(JSON.stringify(data))
